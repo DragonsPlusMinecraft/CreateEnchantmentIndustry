3727f4eef78f052050260372095d61cd0eb42224
LimonBlaze <1195384759@qq.com>
2022-11-03 21:36:12 +0800

    Add config for Deployer experience drop

--- src/main/java/plus/dragons/createenchantmentindustry/foundation/config/CeiServerConfig.java
+++ src/main/java/plus/dragons/createenchantmentindustry/foundation/config/CeiServerConfig.java
@@ -5,23 +5,26 @@ import com.simibubi.create.foundation.config.ui.ConfigAnnotations;
 import net.minecraftforge.common.ForgeConfigSpec;
 
 public class CeiServerConfig extends ConfigBase {
-
-    public final ConfigInt disenchanterTankCapacity = i(1500, 0,
-            "disenchanterTankCapacity",
-            Comments.disenchanterTankCapacity,
-            ConfigAnnotations.RequiresRestart.SERVER.asComment());
-    public final ConfigInt copierTankCapacity = i(3000, 0,
-            "copierTankCapacity",
-            Comments.copierTankCapacity,
-            ConfigAnnotations.RequiresRestart.SERVER.asComment());
-    public final ConfigInt blazeEnchanterTankCapacity = i(3000, 0,
-            "blazeEnchanterTankCapacity",
-            Comments.blazeEnchanterTankCapacity,
-            ConfigAnnotations.RequiresRestart.SERVER.asComment());
+    
+    public final ConfigInt disenchanterTankCapacity = i(1000, 0,
+        "disenchanterTankCapacity",
+        Comments.disenchanterTankCapacity,
+        ConfigAnnotations.RequiresRestart.SERVER.asComment());
+    public final ConfigInt copierTankCapacity = i(4000, 0,
+        "copierTankCapacity",
+        Comments.copierTankCapacity,
+        ConfigAnnotations.RequiresRestart.SERVER.asComment());
+    public final ConfigInt blazeEnchanterTankCapacity = i(2000, 0,
+        "blazeEnchanterTankCapacity",
+        Comments.blazeEnchanterTankCapacity,
+        ConfigAnnotations.RequiresRestart.SERVER.asComment());
     public final ConfigInt maxHyperEnchantingLevelExtension = i(2, 0,
-            "maxHyperEnchantingLevelExtension",
-            Comments.maxHyperEnchantingLevelExtension);
-
+        "maxHyperEnchantingLevelExtension",
+        Comments.maxHyperEnchantingLevelExtension);
+    public final ConfigFloat deployerXpDropChance = f(1, 0, 1,
+        "deployerXpDropChance",
+        Comments.deployerXpDropChance);
+    
     @Override
     protected void registerAll(ForgeConfigSpec.Builder builder) {
         super.registerAll(builder);
@@ -31,18 +34,20 @@ public class CeiServerConfig extends ConfigBase {
     public String getName() {
         return "server";
     }
-
+    
     private static class Comments {
-
+    
         static String disenchanterTankCapacity =
-                "The Tank Capacity of the Disenchanter";
+            "The Tank Capacity of the Disenchanter";
         static String copierTankCapacity =
-                "The Tank Capacity of the Copier";
+            "The Tank Capacity of the Copier";
         static String blazeEnchanterTankCapacity =
-                "The Tank Capacity of the Blaze Enchanter";
+            "The Tank Capacity of the Blaze Enchanter";
         static String maxHyperEnchantingLevelExtension =
-                "The Maximum Extended Levels beyond Enchantment's Max Level that can be reached through Hyper-Enchanting.";
-
+            "The Maximum Extended Levels beyond Enchantment's Max Level that can be reached through Hyper-Enchanting";
+        static String deployerXpDropChance =
+            "The Chance of whether Deployer-killed entities will drop Nugget of Experience";
+        
     }
 
 }
