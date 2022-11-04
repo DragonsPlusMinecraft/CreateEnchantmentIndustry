885c283d5a4609da57cb5f7eb2f541154b2cf235
LimonBlaze <1195384759@qq.com>
2022-11-04 22:09:40 +0800

    Fix mixin remap

--- src/main/java/plus/dragons/createenchantmentindustry/foundation/mixin/TankSegmentAccessor.java
+++ src/main/java/plus/dragons/createenchantmentindustry/foundation/mixin/TankSegmentAccessor.java
@@ -5,7 +5,7 @@ import com.simibubi.create.foundation.tileEntity.behaviour.fluid.SmartFluidTankB
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
-@Mixin(SmartFluidTankBehaviour.TankSegment.class)
+@Mixin(value = SmartFluidTankBehaviour.TankSegment.class, remap = false)
 public interface TankSegmentAccessor {
     
     @Accessor
