dc813d5da22ba7bb02eaec6f2d1da5d3a1cb76bf
MarbleGateKeeper <marblegatekeeper@outlook.com>
2023-03-23 15:57:19 +0800

    Ponder improvement

--- src/main/java/plus/dragons/createenchantmentindustry/foundation/ponder/content/EnchantmentScenes.java
+++ src/main/java/plus/dragons/createenchantmentindustry/foundation/ponder/content/EnchantmentScenes.java
@@ -96,24 +96,33 @@ public class EnchantmentScenes {
         scene.world.showSection(util.select.fromTo(0, 1, 0, 2, 1, 2), Direction.DOWN);
 
         scene.overlay.showText(40)
-                .text("") // We do not use PonderLocalization. For registerText only
+                .text("")
                 .attachKeyFrame()
                 .placeNearTarget()
                 .pointAt(util.vector.topOf(1, 1, 1));
         scene.idle(40);
-        scene.world.setBlock(util.grid.at(1,1,1), CeiBlocks.BLAZE_ENCHANTER.getDefaultState(),false);
-        scene.world.modifyTileEntity(util.grid.at(1,1,1), BlazeEnchanterBlockEntity.class, be-> be.setTargetItem(enchantingGuide(Enchantments.MENDING,1)));
+
+        scene.world.setBlock(util.grid.at(1, 1, 1), CeiBlocks.BLAZE_ENCHANTER.getDefaultState(), false);
+        scene.world.modifyTileEntity(util.grid.at(1, 1, 1), BlazeEnchanterBlockEntity.class, be -> be.setTargetItem(enchantingGuide(Enchantments.MENDING, 1)));
+
+        scene.overlay.showText(140)
+                .text("")
+                .attachKeyFrame()
+                .placeNearTarget()
+                .pointAt(util.vector.topOf(1, 1, 1));
+        scene.idle(140);
+
         scene.overlay.showControls(new InputWindowElement(util.vector.centerOf(1, 1, 1), Pointing.DOWN).whileSneaking().rightClick()
                 .withItem(CeiItems.ENCHANTING_GUIDE.asStack()), 40);
         scene.idle(50);
         scene.overlay.showText(40)
-                .text("") // We do not use PonderLocalization. For registerText only
+                .text("")
                 .attachKeyFrame()
                 .placeNearTarget()
                 .pointAt(util.vector.topOf(1, 1, 1));
         scene.idle(40);
-        scene.world.setBlock(util.grid.at(1,1,1), AllBlocks.BLAZE_BURNER.getDefaultState().setValue(BlazeBurnerBlock.HEAT_LEVEL, BlazeBurnerBlock.HeatLevel.SMOULDERING),false);
-        scene.overlay.showControls(new InputWindowElement(util.vector.centerOf(1, 1, 1), Pointing.DOWN).rightClick().withWrench().whileSneaking(), 40);
+        scene.world.setBlock(util.grid.at(1, 1, 1), AllBlocks.BLAZE_BURNER.getDefaultState().setValue(BlazeBurnerBlock.HEAT_LEVEL, BlazeBurnerBlock.HeatLevel.SMOULDERING), false);
+        scene.overlay.showControls(new InputWindowElement(util.vector.centerOf(1, 1, 1), Pointing.DOWN).whileSneaking().rightClick().withWrench(), 40);
         scene.idle(40);
     }
 
@@ -197,7 +206,7 @@ public class EnchantmentScenes {
         scene.world.showSection(util.select.fromTo(0, 1, 0, 4, 3, 4), Direction.DOWN);
 
         scene.overlay.showText(60)
-                .text("") // We do not use PonderLocalization. For registerText only
+                .text("")
                 .attachKeyFrame()
                 .placeNearTarget()
                 .pointAt(util.vector.topOf(3, 3, 3));
@@ -208,11 +217,18 @@ public class EnchantmentScenes {
         scene.idle(40);
 
         scene.overlay.showText(80)
-                .text("") // We do not use PonderLocalization. For registerText only
+                .text("")
                 .attachKeyFrame()
                 .placeNearTarget()
                 .pointAt(util.vector.topOf(1, 2, 3));
         scene.idle(90);
+
+        scene.overlay.showText(60)
+                .text(".")
+                .attachKeyFrame()
+                .placeNearTarget()
+                .pointAt(util.vector.topOf(1, 2, 3));
+        scene.idle(70);
     }
 
     public static void handleExperienceNugget(SceneBuilder scene, SceneBuildingUtil util){
@@ -365,7 +381,7 @@ public class EnchantmentScenes {
     }
 
     public static void copy(SceneBuilder scene, SceneBuildingUtil util){
-        scene.title("copy", ""); // We do not use PonderLocalization. For title only
+        scene.title("copy", "");
         scene.configureBasePlate(0, 0, 7);
         scene.scaleSceneView(.68f);
         scene.world.setKineticSpeed(util.select.everywhere(), 32f);
@@ -375,11 +391,16 @@ public class EnchantmentScenes {
 
         scene.overlay.showControls(new InputWindowElement(util.vector.centerOf(2, 3, 2), Pointing.DOWN).rightClick()
                 .withItem(Items.ENCHANTED_BOOK.getDefaultInstance()), 40);
-        scene.world.modifyTileEntity(util.grid.at(2,1,5), CreativeFluidTankTileEntity.class, be -> ((CreativeFluidTankTileEntity.CreativeSmartFluidTank) be.getTankInventory())
+        scene.world.modifyTileEntity(util.grid.at(2, 1, 5), CreativeFluidTankTileEntity.class, be -> ((CreativeFluidTankTileEntity.CreativeSmartFluidTank) be.getTankInventory())
                 .setContainedFluid(new FluidStack(CeiFluids.EXPERIENCE.get().getSource(), 1000)));
-        scene.world.modifyTileEntity(util.grid.at(2,3,2), PrinterBlockEntity.class, be ->
-                be.getCapability(CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY).ifPresent(tank->
-                        tank.fill(new FluidStack(CeiFluids.EXPERIENCE.get().getSource(),3000), IFluidHandler.FluidAction.EXECUTE)));
+        scene.world.modifyTileEntity(util.grid.at(2, 3, 2), PrinterBlockEntity.class, be ->
+                be.getCapability(CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY).ifPresent(tank ->
+                        tank.fill(new FluidStack(CeiFluids.EXPERIENCE.get().getSource(), 3000), IFluidHandler.FluidAction.EXECUTE)));
+        scene.overlay.showText(40)
+                .text("")
+                .attachKeyFrame()
+                .placeNearTarget()
+                .pointAt(util.vector.topOf(2, 3, 2));
         scene.idle(40);
 
         var item = Items.BOOK.getDefaultInstance();
@@ -398,13 +419,18 @@ public class EnchantmentScenes {
 
         scene.overlay.showControls(new InputWindowElement(util.vector.centerOf(2, 3, 2), Pointing.DOWN).rightClick()
                 .withItem(Items.WRITTEN_BOOK.getDefaultInstance()), 40);
-        scene.world.modifyTileEntity(util.grid.at(2,1,5), CreativeFluidTankTileEntity.class, be -> ((CreativeFluidTankTileEntity.CreativeSmartFluidTank) be.getTankInventory())
+        scene.world.modifyTileEntity(util.grid.at(2, 1, 5), CreativeFluidTankTileEntity.class, be -> ((CreativeFluidTankTileEntity.CreativeSmartFluidTank) be.getTankInventory())
                 .setContainedFluid(new FluidStack(CeiFluids.INK.get().getSource(), 1000)));
-        scene.world.modifyTileEntity(util.grid.at(2,3,2), PrinterBlockEntity.class, be ->
-                be.getCapability(CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY).ifPresent(tank->{
+        scene.world.modifyTileEntity(util.grid.at(2, 3, 2), PrinterBlockEntity.class, be ->
+                be.getCapability(CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY).ifPresent(tank -> {
                     tank.drain(3000, IFluidHandler.FluidAction.EXECUTE);
-                    tank.fill(new FluidStack(CeiFluids.INK.get().getSource(),3000), IFluidHandler.FluidAction.EXECUTE);
+                    tank.fill(new FluidStack(CeiFluids.INK.get().getSource(), 3000), IFluidHandler.FluidAction.EXECUTE);
                 }));
+        scene.overlay.showText(40)
+                .text("")
+                .attachKeyFrame()
+                .placeNearTarget()
+                .pointAt(util.vector.topOf(2, 3, 2));
         scene.idle(40);
 
         item = Items.BOOK.getDefaultInstance();
