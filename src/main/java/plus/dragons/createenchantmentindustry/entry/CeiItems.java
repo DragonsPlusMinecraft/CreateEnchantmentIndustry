d2392917dd24db81e7578e8203fb221a987eafc1
MarbleGate <marblegatekeeper@outlook.com>
2023-07-27 12:02:57 +0800

    Merge Creative Mode Tab

--- src/main/java/plus/dragons/createenchantmentindustry/entry/CeiItems.java
+++ src/main/java/plus/dragons/createenchantmentindustry/entry/CeiItems.java
@@ -1,7 +1,11 @@
 package plus.dragons.createenchantmentindustry.entry;
 
+import com.simibubi.create.AllBlocks;
+import com.simibubi.create.AllFluids;
+import com.simibubi.create.AllItems;
 import com.tterrag.registrate.util.entry.ItemEntry;
 import net.minecraft.world.item.Rarity;
+import plus.dragons.createdragonlib.init.FillCreateItemGroupEvent;
 import plus.dragons.createenchantmentindustry.content.contraptions.enchanting.enchanter.EnchantingGuideItem;
 import plus.dragons.createenchantmentindustry.content.contraptions.fluids.experience.ExperienceRotorItem;
 import plus.dragons.createenchantmentindustry.content.contraptions.fluids.experience.HyperExperienceBottleItem;
@@ -22,6 +26,17 @@ public class CeiItems {
 
     public static final ItemEntry<ExperienceRotorItem> EXPERIENCE_ROTOR = REGISTRATE.item("experience_rotor", ExperienceRotorItem::new)
             .register();
+
+    public static void fillCreateItemGroup(FillCreateItemGroupEvent event) {
+        if (event.isBase()) {
+            event.addInsertion(AllBlocks.ITEM_DRAIN, CeiBlocks.DISENCHANTER);
+            event.addInsertion(AllBlocks.SPOUT, CeiBlocks.PRINTER);
+            event.addInsertion(AllBlocks.BLAZE_BURNER, ENCHANTING_GUIDE);
+            event.addInsertion(AllItems.ELECTRON_TUBE, EXPERIENCE_ROTOR);
+            event.addInsertion(AllFluids.CHOCOLATE.get().getBucket(), CeiFluids.INK.get().getBucket());
+            event.addInsertion(AllFluids.CHOCOLATE.get().getBucket(), HYPER_EXP_BOTTLE);
+        }
+    }
 
     public static void register() {}
     
