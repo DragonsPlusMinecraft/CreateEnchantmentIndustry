2750eeace88f449da53549dfae97f380e0919667
MarbleGate <marblegatekeeper@outlook.com>
2023-10-11 11:13:28 +0800

    Recipe workaround of experience rotor Close #119

--- src/main/java/plus/dragons/createenchantmentindustry/content/contraptions/fluids/experience/ExperienceRotorItem.java
+++ src/main/java/plus/dragons/createenchantmentindustry/content/contraptions/fluids/experience/ExperienceRotorItem.java
@@ -5,24 +5,13 @@ import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
 public class ExperienceRotorItem extends Item {
-    
+
     public ExperienceRotorItem(Properties properties) {
         super(properties);
     }
-    
+
     @Override
     public boolean isFoil(@NotNull ItemStack pStack) {
         return true;
     }
-    
-    @Override
-    public boolean hasContainerItem(ItemStack stack) {
-        return true;
-    }
-    
-    @Override
-    public ItemStack getContainerItem(ItemStack itemStack) {
-        return itemStack;
-    }
-    
 }
