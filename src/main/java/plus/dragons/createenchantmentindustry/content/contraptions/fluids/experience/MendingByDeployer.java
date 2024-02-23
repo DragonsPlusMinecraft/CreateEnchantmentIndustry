ad3889f1f2bf008bdea0446b41881e6e6e7fb869
Loris <118999347+l0seq@users.noreply.github.com>
2024-02-23 05:52:41 +0100

    Mend item used in Create deployer (#146)

---
+++ src/main/java/plus/dragons/createenchantmentindustry/content/contraptions/fluids/experience/MendingByDeployer.java
@@ -0,0 +1,52 @@
+package plus.dragons.createenchantmentindustry.content.contraptions.fluids.experience;
+
+import net.minecraft.util.Mth;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.fluids.FluidStack;
+import org.jetbrains.annotations.Nullable;
+import plus.dragons.createenchantmentindustry.entry.CeiFluids;
+
+public class MendingByDeployer {
+    
+    public static boolean canItemBeMended(ItemStack stack) {
+        return stack.isDamaged() && EnchantmentHelper.getItemEnchantmentLevel(Enchantments.MENDING, stack) > 0;
+    }
+    
+    public static int getRequiredAmountForItem(ItemStack stack) {
+        return Mth.ceil(stack.getDamageValue() / stack.getXpRepairRatio());
+    }
+    public static int getNewXp(int xpAmount, ItemStack stack) {
+        int requiredAmount = getRequiredAmountForItem(stack);
+        int afterXp = 0;
+
+        if(requiredAmount % 2 != 0) {
+            requiredAmount -= 1;
+        }
+
+        if(requiredAmount == 1) {
+            afterXp = xpAmount;
+        }
+        else if(requiredAmount > 1 && requiredAmount < xpAmount) {
+            afterXp = xpAmount - requiredAmount;
+        }
+
+        return afterXp;
+
+    }
+    @Nullable
+    public static ItemStack mendItem(int xpAmount, ItemStack stack) {
+        int requiredAmount = getRequiredAmountForItem(stack);
+        int damage = stack.getDamageValue();
+        if(requiredAmount % 2 != 0) {
+            requiredAmount -= 1;
+        }
+        damage -= xpAmount * 2;
+        stack.setDamageValue(damage);
+        return stack;
+    }
+
+
+}
--- src/main/java/plus/dragons/createenchantmentindustry/foundation/mixin/DeployerFakePlayerMixin.java
+++ src/main/java/plus/dragons/createenchantmentindustry/foundation/mixin/DeployerFakePlayerMixin.java
@@ -10,18 +10,30 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import plus.dragons.createenchantmentindustry.foundation.config.CeiConfigs;
-
+import java.util.Arrays;
+import plus.dragons.createenchantmentindustry.content.contraptions.fluids.experience.MendingByDeployer;
 @Mixin(value = DeployerFakePlayer.class, remap = false)
 public class DeployerFakePlayerMixin {
 
     @Inject(method = "deployerKillsDoNotSpawnXP", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/event/entity/living/LivingExperienceDropEvent;setCanceled(Z)V"))
     private static void deployerKillsSpawnXpNuggets(LivingExperienceDropEvent event, CallbackInfo ci) {
-        int xp = event.getDroppedExperience();
-        if (xp <= 0) return;
+
         DeployerFakePlayer player = (DeployerFakePlayer) event.getAttackingPlayer();
         assert player != null;
         if (player.getRandom().nextFloat() > CeiConfigs.SERVER.deployerXpDropChance.getF())
             return;
+
+
+        ItemStack deployerTool = player.getInventory().getItem(0);
+        int xp = event.getDroppedExperience();
+
+        if(MendingByDeployer.canItemBeMended(deployerTool)) {
+                player.getInventory().setItem(0, MendingByDeployer.mendItem(xp, deployerTool));
+                xp = MendingByDeployer.getNewXp(xp, deployerTool);
+                event.setDroppedExperience(xp);
+        }
+
+
         int amount = xp / 3 + (player.getRandom().nextInt(3) < xp % 3 ? 1 : 0);
         if (amount <= 0) return;
         Item nugget = AllItems.EXP_NUGGET.get();
